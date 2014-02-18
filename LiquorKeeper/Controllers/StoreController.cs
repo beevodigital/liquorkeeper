0268eee3c81cebd80f41e653a6e7bb5b85bc9e0a
bewebdev <bewebdev@gmail.com>
2014-02-17 21:18:22 -0600

    inital store addition

---
+++ LiquorKeeper/Controllers/StoreController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LiquorKeeper.Models;
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace LiquorKeeper.Controllers
+{
+    public class StoreController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            return View(db.Stores.ToList());
+        }
+
+        // GET: /Store/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // GET: /Store/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Store/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,IsPrimary,Address,City,State,Zip,PrimaryPhone,Hours")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                var ThisUserId = User.Identity.GetUserId();
+                var ThisUser = db.Users.Where(x => x.Id.Equals(ThisUserId)).FirstOrDefault();
+                store.ID = Guid.NewGuid();
+                store.User = ThisUser;
+                db.Stores.Add(store);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(store);
+        }
+
+        // GET: /Store/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: /Store/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,IsPrimary,Address,City,State,Zip,PrimaryPhone,Hours")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }
+
+        // GET: /Store/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: /Store/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Store store = db.Stores.Find(id);
+            db.Stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
